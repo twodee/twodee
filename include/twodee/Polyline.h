19e859c3dc35e3a00717ec66f73e7269de0da668
Chris Johnson <crjjrc@gmail.com>
2017-06-07 13:42:57 -0500

    Remove auto-coalesce from revolve.

--- include/twodee/Polyline.h
+++ include/twodee/Polyline.h
@@ -193,7 +193,7 @@ Trimesh *Polyline<T>::Revolve(const QVector3<T>& axis, int nstops, float degrees
     }
   }
 
-  base->CoalesceVertices(3);
+  /* base->CoalesceVertices(3); */
 
   return base;
 }
